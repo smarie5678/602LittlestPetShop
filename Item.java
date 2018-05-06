47d28a6dfe88e9447c08bb8ee551cc638e10f66a
russndi <38705841+russndi@users.noreply.github.com>
2018-05-06 18:19:46 -0500

    Add files via upload
    recent item class

--- Item.java
+++ Item.java
@@ -3,12 +3,68 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package possystem;
+import java.util.*;
 
 /**
  *
  * @author russelndip
  */
 public class Item {
+    private double price;
+    private double skew;
+    private String supplier;
+    private String description;
+    private int quantity;
+    private int threshold;
+    private boolean reorder;
+    private Date lastOrderDate;
+    Item left;
+    Item right;
+           
+    public Item( double upc, String descr, int quantity, double price)
+    {
+        this.skew = upc;
+        description = descr;
+        this.quantity = quantity;
+        this.price = price;
+        
+        
+    }
+    
+    public void updateQuantity(int amt)
+    {
+        this.quantity = amt;
+    }
+    
+    public double getPrice()
+    {
+          return price;
+    }
+    
+    public double getSkew()
+    {
+        return skew;
+    }
+    public void changePrice(double newPrice)
+    {
+        this.price = newPrice;
+    }
+     public void setThreshhold(int threshold)
+     {
+         this.threshold = threshold;
+ 
+     }
+     
+     public String toString()
+     {
+        String result;
+        result = skew + "\n";
+        result += description + "\n";
+        
+        return result;
+     }
+    
     
 }
